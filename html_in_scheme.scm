44b869918abcf7bed16ef428a96cb42cffc14ae6
jloughry <joe.loughry@gmail.com>
2012-08-31 17:05:13 -0600

    playing around with it

--- html_in_scheme.scm
+++ html_in_scheme.scm
@@ -5,20 +5,54 @@
 ; easy in CL using "rest" but let's see if we can make it work in Scheme with macros
 ; instead...
 
-(html
-	(head
-		(meta (http-equiv (Content-Type content=text/html; charset=utf-8)))
+; (html
+;	(head
+;		(meta (http-equiv (Content-Type "content=text/html; charset=utf-8")))
+;
+;		; Note how quotation marks are unneeded around the string; string is processed as a list.
+;		(title This is the Title))
+;	(body
+;		(h1 This is a heading.)
+;		(p This is a paragraph.)
+;		(p This is a (b paragraph with (i embedded markup)).)))
 
-		; Note how quotation marks are unneeded around the string; string is processed as a list.
-		(title This is the Title))
-	(body
-		(h1 This is a heading.)
-		(p This is a paragraph.)
-		(p This is a (b paragraph with (i embedded markup)).)))
-
-; simplest implementation
+; simplest implementation, not working yet.
 
 (define html
 	(lambda (l)
 		(display (string-append "<html>" l "</html>"))))
+
+(define head
+	(lambda (l)
+		(display (string-append "<head>" l "</head>"))))
+
+(define title
+	(lambda (l)
+		(display (string-append "<title>" l "</title>"))))
+
+(define body
+	(lambda (l)
+		(display (string-append "<body>" l "</body>"))))
+
+(html
+	(head
+		(title "This is a title"))
+	(body "This is the body"))
+
+
+; (define meta
+;	(lambda (l)
+;		(display (string-append "<meta>" l "</meta>"))))
+
+; (define http-equiv
+;	(lambda (l)
+;		(display (string-append "<meta>" l "</meta>"))))
+
+; (define Content-Type
+;		(display "Content-Type "))
+
+; (define p
+;	(lambda (l)
+;		(display (string-append "<p>" l "</p>"))))
+
 
