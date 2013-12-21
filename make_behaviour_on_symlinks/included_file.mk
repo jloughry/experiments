9bfb5b6a1b3596984618aeb44400cacb04076d2f
jloughry <joe.loughry@gmail.com>
2013-12-21 15:55:18 -0700

    makefile and symlinks experiment: proof that it doesn't work

---
+++ make_behaviour_on_symlinks/included_file.mk
@@ -0,0 +1,11 @@
+#
+# This is a file to be included by Make, but Make will only see
+# a symlink to this file. Will it correctly determine that the
+# build is out of date if this file is changed? Make will have
+# to look through the symlink to do it.
+#
+
+can_I_define_variables_here = YES
+
+Makefile: common.mk
+
