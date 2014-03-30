4ed1df349ea6e78d404130871a21fee533ba0704
jloughry <joe.loughry@gmail.com>
2014-03-30 17:31:36 -0600

    implement a queue using a pair of stacks

--- stack/stack.c
+++ stack/stack.c
@@ -16,6 +16,11 @@ payload pop (stack * s);
 void display_stack (stack s);
 payload pop (stack * s);
 stack * create_stack (size_t size);
+int empty (stack * s);
+
+/* implement a queue using a pair of stacks */
+void enqueue(stack * s1, stack * s2, payload d);
+payload dequeue(stack *s1, stack *s2);
 
 stack * create_stack (size_t depth)
 {
@@ -45,11 +50,19 @@ void push (stack * s, payload d)
 	s->top++;
 }
 
+int empty (stack * s)
+{
+	if (0 == s->top) {
+		return 1;
+	}
+	return 0;
+}
+
 payload pop (stack * s)
 {
 	payload return_val = 0;
 
-	if (0 == s->top) {
+	if (empty(s)) {
 		fprintf(stderr, "in pop() at line %d, stack is empty\n", __LINE__);
 	}
 	else {
@@ -64,26 +77,76 @@ void display (stack * s)
 {
 	int i = 0;
 
-	if (s->top == 0) {
+	if (empty(s)) {
 		printf("The stack is empty.\n");
 	}
 	else {
-		for (i=0; i<s->top; i++)
+		printf ("The stack contains ");
+		for (i=0; i<s->top; i++) {
 			printf("%d ", s->contents[i]);
+		}
 		printf ("\n");
 	}
 }
 
 void destroy_stack (stack ** s)
 {
+	if (*s) {
+		if ((*s)->contents) {
+			free((*s)->contents);
+			(*s)->depth = 0;
+			(*s)->top = 0;
+			*s = NULL;
+		}
+		else {
+			printf("Error: in destroy_stack() at line %d, contents pointer was NULL\n", __LINE__);
+		}
+	}
+	else {
+		printf("Error: in destroy_stack() at line %d, *s is NULL\n", __LINE__);
+	}
 	return;
+}
+
+void enqueue(stack * s1, stack * s2, payload d) {
+	push(s1, d);
+}
+
+payload dequeue(stack *s1, stack *s2) {
+	while(!empty(s1)) {
+		push(s2, pop(s1));
+	}
+	return pop(s2);
 }
 
 int main (void)
 {
 	stack * s = NULL;
+	stack * q1 = NULL;
+	stack * q2 = NULL;
 
 	s = create_stack(10);
+
+	/* implement a queue using a pair of stacks */
+
+	q1 = create_stack(100);
+	q2 = create_stack(100);
+
+	/* enqueue some values */
+
+	enqueue(q1, q2, 10);
+	enqueue(q1, q2, 20);
+	enqueue(q1, q2, 30);
+
+	/* dequeue them in the same order */
+	
+	printf("dequeing %d\n", dequeue(q1, q2));
+	printf("dequeing %d\n", dequeue(q1, q2));
+	printf("dequeing %d\n", dequeue(q1, q2));
+	printf("trying to dequeue another value %d\n", dequeue(q1, q2));
+
+	/* push some values onto a vanilla stack and then pop them */
+
 	push(s, 11);
 	push(s, 17);
 	push(s, 999);
