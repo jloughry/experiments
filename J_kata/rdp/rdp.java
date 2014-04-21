2ff80f7162167641ff5a5378d056512744c03bae
jloughry <joe.loughry@gmail.com>
2014-04-21 13:48:19 -0600

    command line arguments and argv[0] (which is not portable beyond Sun's Java implementation)

--- J_kata/rdp/rdp.java
+++ J_kata/rdp/rdp.java
@@ -1,7 +1,18 @@
 public class rdp {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.exit(0);
+	public static void main(String[] argv) {
+		String programme_name = System.getProperty("sun.java.command").split(" ")[0];
+		String input_filename = "";
+
+		switch(argv.length) {
+			case 1:
+				input_filename = argv[0];
+				break;
+			default:
+				System.out.println("Usage: java " + programme_name + " [file]");
+				System.exit(1);
+				break;
+		}
+		System.out.println("input_filename = \"" + input_filename + "\"");
 	}
 }
 
