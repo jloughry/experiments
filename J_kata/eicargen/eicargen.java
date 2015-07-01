3e38a5c66051c88c68a576505519e36731598a3a
jloughry <joe.loughry@gmail.com>
2015-06-30 19:42:50 -0600

    need to escape the backslash in EICAR string

--- J_kata/eicargen/eicargen.java
+++ J_kata/eicargen/eicargen.java
@@ -2,7 +2,6 @@ public class eicargen {
 	public static void main(String[] argv) {
         String programme_name = System.getProperty("sun.java.command").split(" ")[0];
         String argument = "";
-        String secret_string;
 
         switch (argv.length) {
             case 1:
@@ -14,38 +13,30 @@ public class eicargen {
                 break;
         }
 
-        System.out.println("argument = \"" + argument + "\"");
+        if (argument == "write") {
+            System.out.println("obfuscated_data = \"" + obfuscated_data() + "\"");
+        }
+    }
+
+    static public String obfuscated_data() {
+        String c = "4\\PZ";
+        String e = "^)7CC";
+        double pi = 3.14159;
+        String m = "ILE!$";
+        String f = ")7}$E";
+        String b = "%@AP[";
+        String d = "X54(P";
+        String a = "X5O!P";
+        String h = "STAND";
+        Boolean this_is_only_here_to_break_up_the_data_segment;
+        String i = "ARD-A";
+        String g = "ICAR-";
+        String k = "RUS-T";
+        String j = "NTIVI";
+        String n = "H+H*";
+        String l = "EST-F";
+
+        return a+b+c+d+e+f+g+h+i+j+k+l+m+n;
     }
 }
 
-/*
-
-static public String obfuscated_data() {
-    String j = "$EI";
-    String n = "ARD";
-    String f = "54(";
-    String p = "TIV";
-    String u = "LE!";
-    String b = "!P%";
-    String t = "-FI";
-    String v = "$H+";
-    String l = "-ST";
-    String w = "H*";
-    String d = "[4\";
-    String i = ")7}";
-    String c = "@AP";
-    String m = "AND";
-    String o = "-AN";
-    String h = "7CC";
-    String a = "X5O";
-    String q = "IRU";
-    String g = "P^)";
-    String r = "S-T";
-    String s = "EST";
-    String e = "PZX";
-    String k = "CAR";
-
-    return a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v;
-}
-
-*/
